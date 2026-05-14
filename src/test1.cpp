9a9bf8732b33593dde39b7b6eba5602b75031ba8
ConfusedKnight <aarushdangol2006@gmail.com>
2026-05-14 18:49:26 +0545

    drawing circle in openGL

--- src/test1.cpp
+++ src/test1.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <cmath>
+
+#define PI 3.14159
 
 GLFWwindow* StartGLFW();
+void DrawCircle();
 
 int main(){
 
@@ -9,6 +13,7 @@ int main(){
 
   while(!glfwWindowShouldClose(window)){
 
+    DrawCircle();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -21,6 +26,7 @@ int main(){
 }
 
 GLFWwindow* StartGLFW(){
+
   if(!glfwInit()){
 
     std::cerr << "Failed to initialize glfw" << std::endl;
@@ -34,5 +40,37 @@ GLFWwindow* StartGLFW(){
     return nullptr;
   }
 
+  glfwMakeContextCurrent(window);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, 800, 0, 600, -1, 1);
+
+  glMatrixMode(GL_MODELVIEW);
+
   return window;
+}
+
+
+void DrawCircle(){
+
+  int screenWidth = 800.0f, screenHeight = 600.0f;
+
+  float centerX = screenWidth / 2.0f;
+  float centerY = screenHeight / 2.0f;
+  float radius = 50.0f;
+  float res = 100;
+
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex2f(centerX, centerY);
+
+  for(int i=0; i<=res; i++){
+    float angle = 2.0f * PI * i / res;
+    float x = centerX + cos(angle) * radius; 
+    float y = centerY + sin(angle) * radius;
+    glVertex2f(x,y);
+  }
+
+  glEnd();
 }
